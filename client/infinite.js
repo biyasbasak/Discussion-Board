221d337a21a4d7eaf8495a6429615fcbeb7d7a13
biyas basak <biyas@localhost.localdomain>
2016-08-26 18:23:47 +0530

    color design changed

---
+++ client/infinite.js
@@ -0,0 +1,13 @@
+// Template.Comments.onCreated(function() {
+//     let _this = this;
+//     let itemCounter = 10;
+//     Session.setDefault('itemsLimit', itemCounter);
+//     _this.autorun(function() {
+//         _this.subscribe('items', Session.get('itemsLimit'));
+//     });
+// });
+//
+// Template.Comments.helpers({
+//   comments: function() {
+//     return items.find();
+// });
--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,6 @@
 Meteor.publish('comments', function () {
   return Comments.find({});
 });
+Meteor.publish('UserFavourites', function (id) {
+  return Meteor.users.find({_id: id});
+});
